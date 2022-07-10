5beff919fadf9dadc4c4b3c5de90ead487c12f59
kikawet <lopezortegafrancisco1@gmail.com>
2022-07-11 00:49:56 +0200

    redirect to video list

--- src/main/java/com/example/http2/restapi/DefaultController.java
+++ src/main/java/com/example/http2/restapi/DefaultController.java
@@ -1,14 +1,14 @@
 package com.example.http2.restapi;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller 
 public class DefaultController {
 
     @GetMapping
     public String defaultRequest() {
-        return "Hello world";
+        return "redirect:/video";
     }
 
 }
