3048b113fd754f84c39d07b4ede47aeca8f5c037
kikawet <lopezortegafrancisco1@gmail.com>
2022-07-12 00:48:33 +0200

    return basic mediatype

--- src/main/java/com/example/http2/restapi/VideoController.java
+++ src/main/java/com/example/http2/restapi/VideoController.java
@@ -7,7 +7,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeType;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,16 +33,21 @@ public class VideoController {
 
 	@GetMapping("/{partialName}")
 	public ResponseEntity<StreamingResponseBody> getVideo(@PathVariable String partialName) throws IOException {
-		Optional<Path> videoPath = videos.getVideoPathFromName(partialName);
+		Optional<Path> videoPathOpt = videos.getVideoPathFromName(partialName);
 
-		if (!videoPath.isPresent())
+		if (!videoPathOpt.isPresent())
 			return ResponseEntity.notFound().build();
 
+		Path videoPath = videoPathOpt.get();
+
 		StreamingResponseBody responseBody = outputStream -> {
-			Files.copy(videoPath.get(), outputStream);
+			Files.copy(videoPath, outputStream);
 		};
 
+		String mimeType = StringUtils.getFilenameExtension(videoPath.getFileName().toString());
+
 		return ResponseEntity.ok()
+				.contentType(MediaType.asMediaType(MimeType.valueOf("video/" + mimeType)))
 				.body(responseBody);
 	}
 }
