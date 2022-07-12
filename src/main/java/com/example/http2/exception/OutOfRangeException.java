8bdf00ef3799f1a91eb4421ef26c90d844f3c9dc
kikawet <lopezortegafrancisco1@gmail.com>
2022-07-13 01:21:38 +0200

    clean OutOfRangeException

--- src/main/java/com/example/http2/exception/OutOfRangeException.java
+++ src/main/java/com/example/http2/exception/OutOfRangeException.java
@@ -1,9 +1,7 @@
 package com.example.http2.exception;
 
-import java.lang.reflect.Parameter;
-
 public class OutOfRangeException extends IllegalArgumentException {
-	public OutOfRangeException(final Object value,final Object min,final Object max) {
+	public OutOfRangeException(final Object value, final Object min, final Object max) {
 		this(buildErrorMsg(value, min, max));
 	}
 
@@ -11,7 +9,7 @@ public class OutOfRangeException extends IllegalArgumentException {
 		super(msg);
 	}
 
-	private static String buildErrorMsg(final Object value,final Object min,final Object max) {
+	private static String buildErrorMsg(final Object value, final Object min, final Object max) {
 
 		String minStr = min != null ? min.toString() : "-∞";
 		String maxStr = max != null ? max.toString() : "+∞";
