6c1efd659c0a024321e71ee72080d58e8ef384df
kikawet <lopezortegafrancisco1@gmail.com>
2022-07-13 15:44:03 +0200

    refactor VideoService with VideoResourceLoader

---
+++ src/main/java/com/example/http2/beans/VideoResourceLoader.java
@@ -0,0 +1,40 @@
+package com.example.http2.beans;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VideoResourceLoader extends FileSystemResourceLoader {
+
+	@Override
+	public Resource getResource(String path) {
+		return super.getResource(path);
+	}
+
+	public Stream<Path> listFiles(Path path) {
+		try {
+			return Files.list(path)
+					.parallel()
+					.filter(file -> !Files.isDirectory(file));
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to access: " + path.getFileName().toString(), e);
+		}
+	}
+
+	public Stream<Path> listFiles(String path) {
+		return listFiles(Paths.get(path));
+	}
+
+	@Override
+	public ClassLoader getClassLoader() {
+		return super.getClassLoader();
+	}
+
+}
--- src/main/java/com/example/http2/service/VideoService.java
+++ src/main/java/com/example/http2/service/VideoService.java
@@ -1,7 +1,7 @@
 package com.example.http2.service;
 
-import java.io.IOException;
-import java.nio.file.Files;
+import static org.springframework.util.StringUtils.stripFilenameExtension;
+
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -9,10 +9,10 @@ import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import com.example.http2.beans.VideoResourceLoader;
 import com.example.http2.exception.ResourceNotFoundException;
 import com.example.http2.record.Page;
 
@@ -24,39 +24,31 @@ public class VideoService {
 	private static final Page defaulPagination = Page.from(1, 5);
 
 	@Autowired
-	private ResourceLoader resourceLoader;
+	private VideoResourceLoader resourceLoader;
 
 	public Resource getVideoByName(String fileName) {
-		Optional<Path> videoPath = Optional.empty();
 
-		try (Stream<Path> stream = Files.list(videoBasePath)) {
-			videoPath = stream.parallel()
-					.filter(file -> StringUtils.stripFilenameExtension(file.getFileName().toString())
-							.equals(fileName))
-					.findAny();
-		} catch (IOException e) {
-			throw new RuntimeException("Unable to access videos folder", e);
-		}
+		Optional<Path> videoPath = resourceLoader
+				.listFiles(videoBasePath)
+				.parallel()
+				.filter(file -> stripFilenameExtension(file.getFileName().toString())
+						.equals(fileName))
+				.findAny();
 
 		if (!videoPath.isPresent()) {
 			throw new ResourceNotFoundException();
 		}
 
-		return this.resourceLoader.getResource("file:" + videoPath.get());
+		return resourceLoader.getResource(videoPath.get().toString());
 	}
 
 	public Stream<String> getAllVideos(final Page page) {
-		try {
-			return Files.list(videoBasePath)
-					.filter(file -> !Files.isDirectory(file))
-					.skip(page.skip())
-					.limit(page.limit())
-					.map(Path::getFileName)
-					.map(Path::toString)
-					.map(StringUtils::stripFilenameExtension);
-		} catch (IOException e) {
-			throw new InternalError("Unable to access videos folder", e);
-		}
+		return resourceLoader.listFiles(videoBasePath)
+				.skip(page.skip())
+				.limit(page.limit())
+				.map(Path::getFileName)
+				.map(Path::toString)
+				.map(StringUtils::stripFilenameExtension);
 	}
 
 	public Stream<String> getAllVideos() {
