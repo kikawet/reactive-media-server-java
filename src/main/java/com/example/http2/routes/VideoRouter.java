35769a67d906fc110796df93edf80496a6f4ec1f
kikawet <lopezortegafrancisco1@gmail.com>
2022-07-13 23:24:25 +0200

    rename Class VideoRoutes to VideoRouter

--- src/main/java/com/example/http2/routes/VideoRouter.java
+++ src/main/java/com/example/http2/routes/VideoRouter.java
@@ -25,7 +25,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Configuration
-public class VideoRoutes {
+public class VideoRouter {
 
 	@Autowired
 	private VideoService videos;
